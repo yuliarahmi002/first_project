67da368bf7ff3d07bcc9fed2465f9b68c26470b1
yuliarahmi002 <47748161+yuliarahmi002@users.noreply.github.com>
2019-02-21 21:43:00 +0700

    some init msg

---
+++ BasicR.R
@@ -0,0 +1,18 @@
+#Basic R.R
+
+#Yulia Rahmi
+
+#Latihan Dasar-Dasar R
+
+#hobi saya
+
+v_hobi_saya=c("travelling","membaca","menulis")
+
+#matrix_bilangan_ganjil
+
+data_ganjil=c(1,3,5,7,9,11)
+
+matrix_ganjil_saya=matrix(c(1,3,5,7,9,11),byrow=T,nrow=2)
+
+
+#data_frame
